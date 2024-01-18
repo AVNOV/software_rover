f07937d03afdf51746578ad0664c34268f91aa89
Edouard <edouard.laffont@live.com>
2024-01-18 15:01:18 +0100

    fix: synthax rover test

--- tests/Rover/rover.test.ts
+++ tests/Rover/rover.test.ts
@@ -16,30 +16,30 @@ describe("Rover class", () => {
   describe("moveForward function", () => {
     test("should move forward and return his position (0, 1)", () => {
       const rover = new Rover(new Position(0, 0), Orientation.North, map);
-      const roverPostion: Position = rover.moveForward(new Position(1, 1));
+      const roverPosition: Position = rover.moveForward(new Position(1, 1));
       const expectedResult = new Position(0, 1);
-      expect(roverPostion).toEqual(expectedResult);
+      expect(roverPosition).toEqual(expectedResult);
     });
     test("should not move because of obstacle and send his position (0, 0)", () => {
       const rover = new Rover(new Position(0, 0), Orientation.North, map);
-      const roverPostion: Position = rover.moveForward(new Position(0, 1));
+      const roverPosition: Position = rover.moveForward(new Position(0, 1));
       const expectedResult = new Position(0, 0);
-      expect(roverPostion).toEqual(expectedResult);
+      expect(roverPosition).toEqual(expectedResult);
     });
   });
   
   describe("moveBackward function", () => {
     test("should move forward and return his position (0, 5)", () => {
       const rover = new Rover(new Position(0, 0), Orientation.North, map);
-      const roverPostion: Position = rover.moveBackward(new Position(1, 1));
+      const roverPosition: Position = rover.moveBackward(new Position(1, 1));
       const expectedResult = new Position(0, 4);
-      expect(roverPostion).toEqual(expectedResult);
+      expect(roverPosition).toEqual(expectedResult);
     });
     test("should not move because of obstacle and send his position (0, 0)", () => {
       const rover = new Rover(new Position(0, 0), Orientation.North, map);
-      const roverPostion: Position = rover.moveBackward(new Position(0, 0));
+      const roverPosition: Position = rover.moveBackward(new Position(0, 0));
       const expectedResult = new Position(0, 4);
-      expect(roverPostion).toEqual(expectedResult);
+      expect(roverPosition).toEqual(expectedResult);
     });
   });
 
@@ -48,9 +48,9 @@ describe("Rover class", () => {
 
     for (let index = 0; index < orientations.length; index++) {
       test(`should return orientation of the rover (${orientations[index]})`, () => {
-        const roverOriention = rover.turnLeft();
+        const roverOrientation = rover.turnLeft();
         const expectedResult = orientations[index];
-        expect(roverOriention).toEqual(expectedResult);
+        expect(roverOrientation).toEqual(expectedResult);
       });
     }
   });
@@ -60,9 +60,9 @@ describe("Rover class", () => {
 
     for (let index = orientations.length - 1; index >= 0; index--) {
       test(`should return orientation of the rover (${orientations[index]})`, () => {
-        const roverOriention = rover.turnRight();
+        const roverOrientation = rover.turnRight();
         const expectedResult = orientations[index];
-        expect(roverOriention).toEqual(expectedResult);
+        expect(roverOrientation).toEqual(expectedResult);
       });
     }
   });
