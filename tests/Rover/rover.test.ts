4f85b409b292192a9fd9c5ccd9b7c393681bdf58
Corentin Faget <faget.corentin@gmail.com>
2024-01-19 13:40:33 +0100

    feat: improve rover tests

--- tests/Rover/rover.test.ts
+++ tests/Rover/rover.test.ts
@@ -14,31 +14,43 @@ describe("Rover class", () => {
   const map = new Map(5, 5);
 
   describe("moveForward function", () => {
-    test("should move forward and return his position (0, 1)", () => {
+    test("when rover move forward, his position should change and he should return his position (0, 1)", () => {
       const rover = new Rover(new Position(0, 0), Orientation.North, map);
       const roverPosition = rover.moveForward(new Position(1, 1)).position;
       const expectedResult = new Position(0, 1);
       expect(roverPosition).toEqual(expectedResult);
     });
-    test("should not move because of obstacle and send his position (0, 0)", () => {
+    test("when rover move forward to edge of the map, rover should goes at the other end of the map and he should return his position (0, 0)", () => {
+      const rover = new Rover(new Position(0, 4), Orientation.North, map);
+      const roverPosition = rover.moveForward(new Position(1, 1)).position;
+      const expectedResult = new Position(0, 0);
+      expect(roverPosition).toEqual(expectedResult);
+    });
+    test("when rover move forward and an obstacle is in front of him, his position should not change and he should return his position (0, 0)", () => {
       const rover = new Rover(new Position(0, 0), Orientation.North, map);
       const roverPosition = rover.moveForward(new Position(0, 1)).position;
       const expectedResult = new Position(0, 0);
       expect(roverPosition).toEqual(expectedResult);
     });
   });
-  
+
   describe("moveBackward function", () => {
-    test("should move forward and return his position (0, 5)", () => {
+    test("when rover move backward, his position should change and he should return his position (0, 0)", () => {
+      const rover = new Rover(new Position(0, 1), Orientation.North, map);
+      const roverPosition = rover.moveBackward(new Position(1, 1)).position;
+      const expectedResult = new Position(0, 0);
+      expect(roverPosition).toEqual(expectedResult);
+    });
+    test("when rover move backward to edge of the map, rover should goes at the other end of the map and he should return his position (0, 4)", () => {
       const rover = new Rover(new Position(0, 0), Orientation.North, map);
       const roverPosition = rover.moveBackward(new Position(1, 1)).position;
       const expectedResult = new Position(0, 4);
       expect(roverPosition).toEqual(expectedResult);
     });
-    test("should not move because of obstacle and send his position (0, 0)", () => {
-      const rover = new Rover(new Position(0, 0), Orientation.North, map);
+    test("when rover move backward and an obstacle is behind him, his position should not change and he should return his position (0, 1)", () => {
+      const rover = new Rover(new Position(0, 1), Orientation.North, map);
       const roverPosition = rover.moveBackward(new Position(0, 0)).position;
-      const expectedResult = new Position(0, 4);
+      const expectedResult = new Position(0, 1);
       expect(roverPosition).toEqual(expectedResult);
     });
   });
@@ -47,7 +59,7 @@ describe("Rover class", () => {
     const rover = new Rover(new Position(0, 0), Orientation.East, map);
 
     for (let index = 0; index < orientations.length; index++) {
-      test(`should return orientation of the rover (${orientations[index]})`, () => {
+      test(`when the rover turn left, he should return the new orientation of the rover (${orientations[index]})`, () => {
         const roverOrientation = rover.turnLeft().orientation;
         const expectedResult = orientations[index];
         expect(roverOrientation).toEqual(expectedResult);
@@ -59,7 +71,7 @@ describe("Rover class", () => {
     const rover = new Rover(new Position(0, 0), Orientation.North, map);
 
     for (let index = orientations.length - 1; index >= 0; index--) {
-      test(`should return orientation of the rover (${orientations[index]})`, () => {
+      test(`when the rover turn right, he should return the new orientation of the rover (${orientations[index]})`, () => {
         const roverOrientation = rover.turnRight().orientation;
         const expectedResult = orientations[index];
         expect(roverOrientation).toEqual(expectedResult);
